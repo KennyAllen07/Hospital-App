eb73a556272086a9d51fc053bfa13006a2eddcb4
KennyAllen07 <ayanfeoluwa.makinde07@gmail.com>
2023-06-14 00:55:38 +0100

    3rd Commit

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Hospital_App.Interface.IService;
 using Hospital_App.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -39,22 +40,40 @@ namespace Hospital_App.Controllers
             return BadRequest(appointment);
         }
 
-        // POST api/<AppointmentController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+       
+        [HttpGet("GetAppointment")]
+       public async Task<IActionResult> GetAppointment(int id)
         {
+            var appointment = await _appointmentService.GetAppointment(id);
+            if(appointment.Success == true)
+            {
+                return Ok(appointment);
+            }
+            return BadRequest(appointment);
         }
 
         // PUT api/<AppointmentController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpGet("GetAllAppointments")]
+        public async Task<IActionResult> GetAllAppointments()
         {
+            var appointments = await _appointmentService.GetAllAppointments();
+            if(appointments.Success == true)
+            {
+                return Ok(appointments);
+            }
+            return BadRequest(appointments);
         }
 
         // DELETE api/<AppointmentController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("DeleteAppointment")]
+       public async Task<IActionResult> DeleteAppointment(int id)
         {
+            var appointment = await _appointmentService.DeleteAppointment(id);
+            if(appointment.Success == true)
+            {
+                return Ok(appointment);
+            }
+            return BadRequest(appointment);
         }
     }
 }
